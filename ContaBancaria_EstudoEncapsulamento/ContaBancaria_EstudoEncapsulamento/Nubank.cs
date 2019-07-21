73fcca2fab79e0b48e8cbf9b793c746d9fb4532d
Gustavo Godinho <gustavogodinho6@msn.com>
2019-07-20 23:45:21 -0300

    primeiro ckeck-in

---
+++ ContaBancaria_EstudoEncapsulamento/ContaBancaria_EstudoEncapsulamento/Nubank.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Course
+{
+    internal class Nubank
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public Nubank(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Nubank(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
